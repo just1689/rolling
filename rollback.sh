def51771fdb026c93b89fcf3ef43555da22d882b
Just1689 <Just1689@gmail.com>
2020-03-09 22:24:36 +0200

    clean up

--- rollback.sh
+++ rollback.sh
@@ -3,17 +3,14 @@ input="history"
 prev=""
 live=$( cat current )
 echo "The last live version was: $live"
+
+
 while IFS= read -r line
 do
-  #echo "$line"
-  # Do the thing
   if [ "$line" == "$live" ]; then
-    #echo "found the  previous version! $prev"
     break;
   fi
-
   prev=$line
-
 done < "$input"
 
 
@@ -21,6 +18,5 @@ if [ "$prev" == "" ]; then
   echo "No previous changes found.. Can't rollback"
   exit 0;
 fi
-
 echo "> Current versio should be: $prev"
 echo "$prev" > current
